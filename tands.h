6758280ed28e3746a2a023196a711c8fa17e70e4
YIFEI-MA <707930340ma@gmail.com>
2020-10-29 16:41:58 -0600

    Initial commit

---
+++ tands.h
@@ -0,0 +1,14 @@
+//
+// Created by Yifei Ma on 2020-10-29.
+//
+
+#ifndef PRODCON_TANDS_H
+#define PRODCON_TANDS_H
+
+#include <stdio.h>
+#include <time.h>
+
+void Trans( int n );
+void Sleep( int n );
+
+#endif //PRODCON_TANDS_H
