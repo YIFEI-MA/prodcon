9087384f54e26445e5c2205a1ad14a057492c494
YIFEI-MA <707930340ma@gmail.com>
2020-10-30 02:36:14 -0600

    add input.txt

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+
+pthread_mutex_t x_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t x_cond = PTHREAD_COND_INITIALIZER;
 
 int main() {
     printf("Hello, World!\n");
